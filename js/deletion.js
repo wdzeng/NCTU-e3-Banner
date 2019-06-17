0f3176108130752b9da372ef489db1334e613d4d
Hyperbola <hyperbola.cs07@gmail.com>
2019-06-17 16:42:54 +0800

    v0 published

--- js/deletion.js
+++ js/deletion.js
@@ -1,2 +1,14 @@
-let c = document.getElementsByName("captcha_code")[0];
-c.parentNode.removeChild(c);
+let ccinputs = document.getElementsByName("captcha_code");
+ccinputs[2].remove();
+ccinputs[1].remove();
+ccinputs[0].remove();
+
+let ccimages = document.getElementsByName("captcha");
+ccimages[2].remove();
+ccimages[1].remove();
+ccimages[0].remove();
+
+let cctexts = document.querySelectorAll("a[href='#']");
+cctexts[2].remove();
+cctexts[1].remove();
+cctexts[0].remove();
